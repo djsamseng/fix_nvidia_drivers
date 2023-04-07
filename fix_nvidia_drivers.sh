3190587af4762d44342a10e27dab8ce37f4b621a
Samuel Seng <djsamseng@gmail.com>
2023-04-07 12:58:46 -0400

    Switch from nvidia driver 470 to 510

--- fix_nvidia_drivers.sh
+++ fix_nvidia_drivers.sh
@@ -4,5 +4,5 @@ echo "Usage: sudo bash ./fix_nvidia_drivers.sh"
 apt-get remove nvidia*
 ubuntu-drivers devices
 ubuntu-drivers autoinstall
-apt-get install nvidia-driver-470
+apt-get install nvidia-driver-510
 echo "Complete: please run 'sudo shutdown -r now'"
